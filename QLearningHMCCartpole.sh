d0d666bf7d27f9488689aa3be7766d3583bbb5a2
Udari Madhushani <udarim@compton.princeton.edu>
2020-09-04 22:49:45 -0400

    cartpole command updated

---
+++ QLearningHMCCartpole.sh
@@ -0,0 +1,3 @@
+python3 QLearningHMCCartpole.py --sdim 4 --adim 1 --srange -0.785 0.785 -3 3 -2.4 2.4 -3.5 3.5 --arange -10 10 --ssize 5 --asize 5 --scov 5 5 5 5 --max-bias 10 --samples 50 --steps 1000 --seed 32 --stepsize 0.02 --trlen 100 --burn 0 --hmcsample 200 --cSig 50 --mode hmc
+
+# python3 QLearningHMCCartpole.py --sdim 4 --adim 1 --srange -0.785 0.785 -3 3 -2.4 2.4 -3.5 3.5 --arange -10 10 --ssize 5 --asize 5 --scov 5 5 5 5 --max-bias 10 --samples 300 --steps 500 --seed 32
